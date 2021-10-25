5ead09c6debc225e3967eae660601cac7bd0eff5
rye12 <akbarmuin91@gmail.com>
2021-10-25 11:10:53 +0700

    v9.7 rye

--- App/Http/Controllers/KendaraanController.php
+++ App/Http/Controllers/KendaraanController.php
@@ -8,11 +8,6 @@ use DB;
 
 class KendaraanController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $data = DB::table('kendaraan as c')
@@ -24,23 +19,12 @@ class KendaraanController extends Controller
         return view('kendaraan.index', compact('data'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
 
         return view('kendaraan.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -61,7 +45,6 @@ class KendaraanController extends Controller
             'warna' => $request->warna,
         ];
         DB::table('kendaraan')->insert($kendaraan);
-        // dd($kendaraan);
         return redirect()->back()->with('success', 'Post updated successfully');
     }
 
@@ -71,12 +54,6 @@ class KendaraanController extends Controller
         return view('kendaraan.show', compact('kendaraan'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Kendaraan  $kendaraan
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $kendaraan = DB::table("kendaraan")->where('id', $id)->first();
@@ -84,57 +61,20 @@ class KendaraanController extends Controller
         return view('kendaraan.edit', compact('kendaraan', 'jenis'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Kendaraan  $kendaraan
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        // dd($kendaraan);
-        $data = $request->validate([
-            'nama' => 'required',
-            'id_jenis' => 'required',
-            'no_rangka' => 'required',
-            'no_plat' => 'required',
-            'no_mesin' => 'required',
-            'warna' => 'required',
-            // 'jenis' => 'required',
-            // 'warna' => 'required',
-            // 'mesin' => 'required',
-            // 'nama' => 'required',
-            // 'plat' => 'required',
-            // 'pemilik'=>'required',
-            // 'password' => 'required',
 
-        ]);
-
-        $kendaraan = DB::table("kendaraan")->where('id', $id)->update([
+        DB::table("kendaraan")->where('id', $id)->update([
             'nama' => $request->nama,
             'id_jenis' => $request->id_jenis,
             'no_rangka' => $request->no_rangka,
             'no_plat' => $request->no_plat,
             'no_mesin' => $request->no_mesin,
             'warna' => $request->warna,
-            // 'jenis' => $request->jenis,
-            // 'warna' => $request->warna,
-            // 'mesin' => $request->mesin,
-            // 'nama' => $request->nama,
-            // 'plat' => $request->plat,
-            // 'pemilik'=>$request->pemilik,
         ]);
-        // dd($kendaraan);
         return redirect()->route('kendaraan.index')->with('success', 'Post updated successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Kendaraan  $kendaraan
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $del = DB::table('kendaraan')->where('id', $id);
